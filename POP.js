3fa201595ebe2099dabc9988c89fcfa98f30826e
LolWebPop <69006818+LolWebPop@users.noreply.github.com>
2020-07-31 04:39:20 +0530

    Update POP.js

--- POP.js
+++ POP.js
@@ -41,7 +41,7 @@ async function opn()
 
 async function vw()
 {
-	var v=(await req("lolwebpop.000webhostapp.com/dat.dat")).split('-'), d=new Date(), t=await req("lolwebpop.000webhostapp.com/Ddat.dat");
+	var v=(await req("lolwebpop.000webhostapp.com/dat.dat")).split`-`.map(x=>+x), d=new Date(), t=parseInt(await req("lolwebpop.000webhostapp.com/Ddat.dat"));
 	document.getElementById("cnt").innerHTML="<U><center><H5>Numbers Of Visitors TODAY("+['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d.getDay()]+") Till Now : "+t+"<BR/>Numbers Of Visitors This MONTH("+["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][d.getMonth()]+") Till Now : "+(v[0]+t)+"<BR/>Numbers Of Visitors This YEAR("+d.getYear()+") Till Now : "+(v[1]+t)+"<BR/>Numbers Of Visitors Till Now : "+(v[2]+t)+"</H5></U></center>";
 }
 
