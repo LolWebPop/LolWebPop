ee73f5e82cd8db1ca973c73a6d2fda70f9dc43d5
LolWebPop <69006818+LolWebPop@users.noreply.github.com>
2020-07-31 03:27:14 +0530

    Add files via upload

---
+++ POP.js
@@ -0,0 +1,47 @@
+var s,l,m=Math;
+
+async function con(a){console.log(a)}
+
+async function bdy()
+{
+	console.log("If you are not an adult or not allowed to access porn according to law of your location then quit right now");
+	alert("This website was made to redirect to other PORNOGRAPHIC WEBSITES...");
+	s = (await req("lolwebpop.github.io/LolWebPop/data.txt")).split('\n');
+	l=s.length;
+	vw();
+	req("lolwebpop.000webhostapp.com/iVisited.php");
+}
+
+function rand(t=Date.now()) { return parseInt(m.abs(m.sin(t)+m.cos(t))*10000000) }
+
+function req(u)
+{
+	var xhr = new XMLHttpRequest();
+	xhr.open("Get", "https://"+u);
+	return new Promise(
+	(r)=>
+	{
+		xhr.onload = () =>
+		{
+			r(xhr.response);
+		};
+		xhr.send();
+	});
+}
+
+async function opn()
+{
+	var URL = s[rand()%l]
+	URL = "www."+URL+(URL.indexOf('.')+1?"":".com")
+	con(URL)
+	var pg=window.open('',"_blank");
+	pg.document.write("<H1>Loading...");
+	pg.location.href=URL;
+}
+
+async function vw()
+{
+	var v=(await req("lolwebpop.000webhostapp.com/dat.dat")).split('-'), d=new Date();
+	document.getElementById("cnt").innerhtml="<U><center><H2>Numbers Of Visitors TODAY("+['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d.getDay()]+") Till Now : "+await req("lolwebpop.000webhostapp.com/Ddat.dat")+"<BR/>Numbers Of Visitors This MONTH("+["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][d.getMonth()]+") Till Now : "+v[0]+"<BR/>Numbers Of Visitors This YEAR("+d.getYear()+") Till Now : "+v[1]+"<BR/>Numbers Of Visitors Till Now : "+v[2]+"</H2></U></center>";
+}
+
